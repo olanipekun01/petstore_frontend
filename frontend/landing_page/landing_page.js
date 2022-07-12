141d726ca5c0731f19303eda88924737180cc801
Eyitayo Olonilua <oloniluaeyitayo@gmail.com>
2022-07-12 04:18:42 -0700

    worked on the categories page

--- frontend/landing_page/landing_page.js
+++ frontend/landing_page/landing_page.js
@@ -6,17 +6,17 @@ let landingHero = document.querySelector('.landing_hero');
 
 
 // let secondSection = document.querySelector('.third_card_div');
-let cleanCatText = document.querySelector('.clean_cat_text');
-let feedCatText = document.querySelector('.feed_cat_text');
-let housingCatText = document.querySelector('.housing_cat_text');
-let petsCatText = document.querySelector('.pets_cat_text');
-let servicesCatText = document.querySelector('.services_cat_text');
+// let cleanCatText = document.querySelector('.clean_cat_text');
+// let feedCatText = document.querySelector('.feed_cat_text');
+// let housingCatText = document.querySelector('.housing_cat_text');
+// let petsCatText = document.querySelector('.pets_cat_text');
+// let servicesCatText = document.querySelector('.services_cat_text');
 
-let cleanCatImg = document.querySelector('.clean_cat_img');
-let feedCatImg = document.querySelector('.feed_cat_img');
-let housingCatImg = document.querySelector('.housing_cat_img');
-let petsCatImg = document.querySelector('.pets_cat_img');
-let servicesCatImg = document.querySelector('.services_cat_img');
+// let cleanCatImg = document.querySelector('.clean_cat_img');
+// let feedCatImg = document.querySelector('.feed_cat_img');
+// let housingCatImg = document.querySelector('.housing_cat_img');
+// let petsCatImg = document.querySelector('.pets_cat_img');
+// let servicesCatImg = document.querySelector('.services_cat_img');
 
 
 shopLink.onmouseover = () => {
@@ -50,42 +50,42 @@ landingHero.onmouseover = () => {
 //     servicesCatText.classList.remove('active');
 // }
 
-cleanCatImg.onmouseover = () => {
-    cleanCatText.classList.add('active');
-    feedCatText.classList.remove('active');
-    housingCatText.classList.remove('active');
-    petsCatText.classList.remove('active');
-    servicesCatText.classList.remove('active');
-}
+// cleanCatImg.onmouseover = () => {
+//     cleanCatText.classList.add('active');
+//     feedCatText.classList.remove('active');
+//     housingCatText.classList.remove('active');
+//     petsCatText.classList.remove('active');
+//     servicesCatText.classList.remove('active');
+// }
 
-feedCatImg.onmouseover = () => {
-    feedCatText.classList.add('active');
-    cleanCatText.classList.remove('active');
-    housingCatText.classList.remove('active');
-    petsCatText.classList.remove('active');
-    servicesCatText.classList.remove('active');
-}
+// feedCatImg.onmouseover = () => {
+//     feedCatText.classList.add('active');
+//     cleanCatText.classList.remove('active');
+//     housingCatText.classList.remove('active');
+//     petsCatText.classList.remove('active');
+//     servicesCatText.classList.remove('active');
+// }
 
-housingCatImg.onmouseover = () => {
-    housingCatText.classList.add('active');
-    feedCatText.classList.remove('active');
-    cleanCatText.classList.remove('active');
-    petsCatText.classList.remove('active');
-    servicesCatText.classList.remove('active');
-}
+// housingCatImg.onmouseover = () => {
+//     housingCatText.classList.add('active');
+//     feedCatText.classList.remove('active');
+//     cleanCatText.classList.remove('active');
+//     petsCatText.classList.remove('active');
+//     servicesCatText.classList.remove('active');
+// }
 
-petsCatImg.onmouseover = () => {
-    petsCatText.classList.add('active');
-    feedCatText.classList.remove('active');
-    cleanCatText.classList.remove('active');
-    housingCatText.classList.remove('active');
-    servicesCatText.classList.remove('active');
-}
+// petsCatImg.onmouseover = () => {
+//     petsCatText.classList.add('active');
+//     feedCatText.classList.remove('active');
+//     cleanCatText.classList.remove('active');
+//     housingCatText.classList.remove('active');
+//     servicesCatText.classList.remove('active');
+// }
 
-servicesCatImg.onmouseover = () => {
-    servicesCatText.classList.add('active');
-    feedCatText.classList.remove('active');
-    cleanCatText.classList.remove('active');
-    housingCatText.classList.remove('active');
-    petsCatText.classList.remove('active');
-}
+// servicesCatImg.onmouseover = () => {
+//     servicesCatText.classList.add('active');
+//     feedCatText.classList.remove('active');
+//     cleanCatText.classList.remove('active');
+//     housingCatText.classList.remove('active');
+//     petsCatText.classList.remove('active');
+// }
